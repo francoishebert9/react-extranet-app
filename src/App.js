1e49c600f22bdf400dc0fd5a13fe0e6868823aa5
Francois Hebert <hebert_francois@hotmail.com>
2018-04-25 20:17:48 -0400

    ::ÉÉÉ''MÇÀ

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// BrowserRouter is the router implementation for HTML5 browsers (vs Native).
+// Link is your replacement for anchor tags.
+// Route is the conditionally shown component based on matching a path to a URL.
+// Switch returns only the first matching route rather than all matching routes.
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import React from 'react';
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Contact = () => <h1>Contact</h1>;
+
+// We give each route either a target `component`, or we can send functions in `render` or `children` 
+// that return valid nodes. `children` always returns the given node whether there is a match or not.
+const App = () => (
+  <Router basename="/react">
+    <div>
+      <Link to="/">Home</Link>{' '}
+      <Link to="/about">About</Link>{' '}
+      <Link to="/contact">Contact</Link>
+      
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </div>
+  </Router>
+);
+export default App
